f62ef0fde6e17510190cb298edb7b3e2c5442405
Katya <basagan13@gmail.com>
2023-08-18 21:20:05 +0300

    added setUp() to test_stack.py

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -2,28 +2,30 @@
 import unittest
 from src.stack import Node, Stack
 
-test1 = Node(3, None)
-test2 = Node('tested', test1)
-test3 = Node(True, 5)
-
-stack1 = Stack()
-stack1.push('da')
-stack1.push(False)
-stack1.push([3, 6, True])
 
 class TestStack(unittest.TestCase):
+    def setUp(self):
+        self.test1 = Node(3, None)
+        self.test2 = Node('tested', self.test1)
+        self.test3 = Node(True, 5)
+
+        self.stack1 = Stack()
+        self.stack1.push('da')
+        self.stack1.push(False)
+        self.stack1.push([3, 6, True])
+
     def test_node(self):
-        self.assertEqual(test1.next_node, None)
-        self.assertEqual(test2.data, 'tested')
-        self.assertEqual(test3.data, True)
+        self.assertEqual(self.test1.next_node, None)
+        self.assertEqual(self.test2.data, 'tested')
+        self.assertEqual(self.test3.data, True)
 
     def test_stack(self):
-        self.assertEqual(stack1.top.data, [3, 6, True])
-        self.assertEqual(stack1.top.next_node.data, False)
-        self.assertEqual(stack1.top.next_node.next_node.data, 'da')
+        self.assertEqual(self.stack1.top.data, [3, 6, True])
+        self.assertEqual(self.stack1.top.next_node.data, False)
+        self.assertEqual(self.stack1.top.next_node.next_node.data, 'da')
 
         with self.assertRaises(AttributeError):
-            stack1.top.next_node.next_node.next_node.data
+            self.stack1.top.next_node.next_node.next_node.data
 
     def test_stack_pop(self):
         stack = Stack()
